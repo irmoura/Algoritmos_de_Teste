fa139e434644ac4c520e304acc10bfee4a420277
Ismael Ribeiro <coragethedogcowardle@gmail.com>
2016-03-15 10:46:51 -0300

    * Alteração do código 001

--- Codigos_de_Teste/src/CODIGOS/Calculadora.java
+++ Codigos_de_Teste/src/CODIGOS/Calculadora.java
@@ -23,7 +23,11 @@ public class Calculadora {
             opcao[i] = JOptionPane.showInputDialog(null,"Digite o "+(i+1)+"º número: ","Calculadora",JOptionPane.QUESTION_MESSAGE);
         }
         
-        operacao = JOptionPane.showInputDialog(null,"Digite a operação desejada: ","Calculadora",JOptionPane.QUESTION_MESSAGE);
+        operacao = JOptionPane.showInputDialog(null,"Digite a operação desejada: "+
+                "\n1 - Subtração"+
+                "\n2 - Soma"+
+                "\n3 - Divisão"+
+                "\n4 - Multiplicação","Calculadora",JOptionPane.QUESTION_MESSAGE);
         
     }
 }
