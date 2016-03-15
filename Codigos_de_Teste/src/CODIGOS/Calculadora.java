32f1e90c0a490214c71b82dfc0285281dca3e9a0
Ismael Ribeiro <coragethedogcowardle@gmail.com>
2016-03-15 11:08:05 -0300

    * Alteração do código 002

--- Codigos_de_Teste/src/CODIGOS/Calculadora.java
+++ Codigos_de_Teste/src/CODIGOS/Calculadora.java
@@ -15,7 +15,7 @@ public class Calculadora {
     
     static int i;
     static String opcao[] = new String[2];
-    static String operacao;
+    static String operacao = "";
     
     public static void main(String args[]){
         
@@ -23,11 +23,17 @@ public class Calculadora {
             opcao[i] = JOptionPane.showInputDialog(null,"Digite o "+(i+1)+"º número: ","Calculadora",JOptionPane.QUESTION_MESSAGE);
         }
         
-        operacao = JOptionPane.showInputDialog(null,"Digite a operação desejada: "+
+        while(!operacao.equals("1") && !operacao.equals("2") && !operacao.equals("3") && !operacao.equals("4")){
+            operacao = JOptionPane.showInputDialog(null,"Digite a operação desejada: "+
                 "\n1 - Subtração"+
                 "\n2 - Soma"+
                 "\n3 - Divisão"+
                 "\n4 - Multiplicação","Calculadora",JOptionPane.QUESTION_MESSAGE);
+            
+            if(!operacao.equals("1") && !operacao.equals("2") && !operacao.equals("3") && !operacao.equals("4")){
+                JOptionPane.showMessageDialog(null,"Digite entre as opções 1 à 4 ...","Aviso",JOptionPane.WARNING_MESSAGE);
+            }
+        }
         
     }
 }
