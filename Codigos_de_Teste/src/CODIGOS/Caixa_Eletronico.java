944632e5e2c28c975d4319a41d97ef30a07f8a2b
irmoura <irmoura@F02DTIBSW1001.ibyte.corp>
2016-08-17 10:19:13 -0300

    * Correção do código ...

--- Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
+++ Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
@@ -13,36 +13,45 @@ import javax.swing.JOptionPane;
  */
 public class Caixa_Eletronico {
     
-    public static int valor_liberado;
+    public static int contador, valor_liberado, valor_solicitado;
     public static int notas_de_10, notas_de_20, notas_de_50, notas_de_100;
     
     public static void main(String args[]){
         
-        String valor_solicitado = JOptionPane.showInputDialog(null,"Digite o valor que deseja sacar :");
+        valor_solicitado = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o valor que deseja sacar :"));
         
-        int valor_soli_int = Integer.parseInt(valor_solicitado);
-        
-        for(int i= 0; i < valor_soli_int; i++){
-            valor_liberado++;
-            if(valor_liberado%10 == 0){
+        for(int i= 0; i < valor_solicitado; i++){
+            contador++;
+            if(contador%10 == 0){
                 notas_de_10++;
             }
-            if(valor_liberado%20 == 0){
+            if(contador%20 == 0){
                 notas_de_20++;
             }
-            if(valor_liberado%50 == 0){
+            if(contador%50 == 0){
                 notas_de_50++;
             }
-            if(valor_liberado%100 == 0){
+            if(contador%100 == 0){
                 notas_de_100++;
             }
+            
+        }
+        
+        if((notas_de_100*100) == valor_solicitado){
+            JOptionPane.showMessageDialog(null,"Quantidade de notas de 100 : "+notas_de_100);
+        }
+        
+        if((notas_de_20*20) < valor_solicitado){
+            JOptionPane.showMessageDialog(null,"As notas de 20 são insuficientes "+(notas_de_20*20)+
+                    " - valor solicitado :"+valor_solicitado);
         }
         
         JOptionPane.showMessageDialog(null,"Quantidade de notas de 10 :"+notas_de_10+"\n"
         +"Quantidade de notas de 20 :"+notas_de_20+"\n"
         +"Quantidade de notas de 50 :"+notas_de_50+"\n"
         +"Quantidade de notas de 100 :"+notas_de_100+"\n"
-        +"Valor solicitado :"+valor_soli_int);
+        +"Valor solicitado :"+valor_solicitado+"\n"
+        +"Valor liberado :"+valor_liberado);
         
     }
 }
