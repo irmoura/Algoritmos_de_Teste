8b044618ad6f27606c6d173a02a03cd0d978911c
Ismael Ribeiro <Ismael Ribeiro@DESKTOP-NHM07EE>
2016-08-17 22:48:57 -0300

    * Correção do código caixa eletronico.

--- Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
+++ Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
@@ -13,45 +13,79 @@ import javax.swing.JOptionPane;
  */
 public class Caixa_Eletronico {
     
-    public static int contador, valor_liberado, valor_solicitado;
-    public static int notas_de_10, notas_de_20, notas_de_50, notas_de_100;
+    public static int valor_solicitado ,notas_de_10, notas_de_20, notas_de_50, notas_de_100;
     
     public static void main(String args[]){
         
-        valor_solicitado = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o valor que deseja sacar :"));
+        for(int j=0; j < 5; j++){
+        
+        valor_solicitado = 0;
+        notas_de_10 = 0;
+        notas_de_20 = 0;
+        notas_de_50 = 0;
+        notas_de_100 = 0;
+            
+        valor_solicitado = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o valor que deseja sacar : "));
         
         for(int i= 0; i < valor_solicitado; i++){
-            contador++;
-            if(contador%10 == 0){
-                notas_de_10++;
+            notas_de_100++;
+            if(notas_de_100*100 > valor_solicitado){
+                notas_de_100--;
             }
-            if(contador%20 == 0){
-                notas_de_20++;
-            }
-            if(contador%50 == 0){
-                notas_de_50++;
-            }
-            if(contador%100 == 0){
-                notas_de_100++;
-            }
-            
         }
         
-        if((notas_de_100*100) == valor_solicitado){
-            JOptionPane.showMessageDialog(null,"Quantidade de notas de 100 : "+notas_de_100);
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_50++;
+            if(notas_de_50*50 > valor_solicitado){
+                notas_de_50--;
+            }
+            if((notas_de_50*50) > (valor_solicitado - notas_de_100*100)){
+                notas_de_50--;
+            }
         }
         
-        if((notas_de_20*20) < valor_solicitado){
-            JOptionPane.showMessageDialog(null,"As notas de 20 são insuficientes "+(notas_de_20*20)+
-                    " - valor solicitado :"+valor_solicitado);
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_20++;
+            if(notas_de_20*20 > valor_solicitado){
+                notas_de_20--;
+            }
+            if((notas_de_20*20) > (valor_solicitado - notas_de_50*50)){
+                notas_de_20--;
+            }
+            if((notas_de_20*20) > (valor_solicitado - notas_de_100*100)){
+                notas_de_20--;
+            }
+            if(((notas_de_20*20)+(notas_de_50*50)+(notas_de_100*100)) > valor_solicitado){
+                notas_de_20--;
+            }
+        }
+        
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_10++;
+            if(notas_de_10*10 > valor_solicitado){
+                notas_de_10--;
+            }
+            if((notas_de_10*10) > (valor_solicitado - notas_de_20*20)){
+                notas_de_10--;
+            }
+            if((notas_de_10*10) > (valor_solicitado - notas_de_50*50)){
+                notas_de_10--;
+            }
+            if((notas_de_10*10) > (valor_solicitado - notas_de_100*100)){
+                notas_de_10--;
+            }
+            if(((notas_de_10*10)+(notas_de_20*20)+(notas_de_50*50)+(notas_de_100*100)) > valor_solicitado){
+                notas_de_10--;
+            }
         }
         
         JOptionPane.showMessageDialog(null,"Quantidade de notas de 10 :"+notas_de_10+"\n"
         +"Quantidade de notas de 20 :"+notas_de_20+"\n"
         +"Quantidade de notas de 50 :"+notas_de_50+"\n"
         +"Quantidade de notas de 100 :"+notas_de_100+"\n"
-        +"Valor solicitado :"+valor_solicitado+"\n"
-        +"Valor liberado :"+valor_liberado);
+        +"Valor solicitado :"+valor_solicitado);
+            
+        }
         
     }
 }
