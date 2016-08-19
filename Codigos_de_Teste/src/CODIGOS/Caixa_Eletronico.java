1761d2f5ace683b702f41bc1953bcfd27ca4d914
irmoura <irmoura@F02DTIBSW1001.ibyte.corp>
2016-08-19 09:26:29 -0300

    * Na classe caixa eletronico, adicionadas as notas de 5, 2 e 1 real.

--- Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
+++ Codigos_de_Teste/src/CODIGOS/Caixa_Eletronico.java
@@ -13,13 +13,17 @@ import javax.swing.JOptionPane;
  */
 public class Caixa_Eletronico {
     
-    public static int valor_solicitado ,notas_de_10, notas_de_20, notas_de_50, notas_de_100;
+    public static int valor_solicitado ,
+    notas_de_1, notas_de_2, notas_de_5, notas_de_10, notas_de_20, notas_de_50, notas_de_100;
     
     public static void main(String args[]){
         
-        for(int j=0; j < 5; j++){
+        for(int j=0; j < 100; j++){
         
         valor_solicitado = 0;
+        notas_de_1 = 0;
+        notas_de_2 = 0;
+        notas_de_5 = 0;
         notas_de_10 = 0;
         notas_de_20 = 0;
         notas_de_50 = 0;
@@ -79,11 +83,89 @@ public class Caixa_Eletronico {
             }
         }
         
-        JOptionPane.showMessageDialog(null,"Quantidade de notas de 10 :"+notas_de_10+"\n"
-        +"Quantidade de notas de 20 :"+notas_de_20+"\n"
-        +"Quantidade de notas de 50 :"+notas_de_50+"\n"
-        +"Quantidade de notas de 100 :"+notas_de_100+"\n"
-        +"Valor solicitado :"+valor_solicitado);
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_5++;
+            if(notas_de_5*5 > valor_solicitado){
+                notas_de_5--;
+            }
+            if((notas_de_5*5) > (valor_solicitado - notas_de_10*10)){
+                notas_de_5--;
+            }
+            if((notas_de_5*5) > (valor_solicitado - notas_de_20*20)){
+                notas_de_5--;
+            }
+            if((notas_de_5*5) > (valor_solicitado - notas_de_50*50)){
+                notas_de_5--;
+            }
+            if((notas_de_5*5) > (valor_solicitado - notas_de_100*100)){
+                notas_de_5--;
+            }
+            if(((notas_de_5*5)+(notas_de_10*10)+(notas_de_20*20)+(notas_de_50*50)+(notas_de_100*100)) > valor_solicitado){
+                notas_de_5--;
+            }
+        }
+        
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_2++;
+            if(notas_de_2*2 > valor_solicitado){
+                notas_de_2--;
+            }
+            if((notas_de_2*2) > (valor_solicitado - notas_de_5*5)){
+                notas_de_2--;
+            }
+            if((notas_de_2*2) > (valor_solicitado - notas_de_10*10)){
+                notas_de_2--;
+            }
+            if((notas_de_2*2) > (valor_solicitado - notas_de_20*20)){
+                notas_de_2--;
+            }
+            if((notas_de_2*2) > (valor_solicitado - notas_de_50*50)){
+                notas_de_2--;
+            }
+            if((notas_de_2*2) > (valor_solicitado - notas_de_100*100)){
+                notas_de_2--;
+            }
+            if(((notas_de_2*2)+(notas_de_5*5)+(notas_de_10*10)+(notas_de_20*20)+(notas_de_50*50)+(notas_de_100*100)) > valor_solicitado){
+                notas_de_2--;
+            }
+        }
+        
+        for(int i= 0; i < valor_solicitado; i++){
+            notas_de_1++;
+            if(notas_de_1 > valor_solicitado){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_2*2)){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_5*5)){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_10*10)){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_20*20)){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_50*50)){
+                notas_de_1--;
+            }
+            if((notas_de_1) > (valor_solicitado - notas_de_100*100)){
+                notas_de_1--;
+            }
+            if(((notas_de_1)+(notas_de_2*2)+(notas_de_5*5)+(notas_de_10*10)+(notas_de_20*20)+(notas_de_50*50)+(notas_de_100*100)) > valor_solicitado){
+                notas_de_1--;
+            }
+        }
+        
+        JOptionPane.showMessageDialog(null,"Quantidade de notas de 1 : "+notas_de_1+"\n"
+                                          +"Quantidade de notas de 2 : "+notas_de_2+"\n"
+                                          +"Quantidade de notas de 5 : "+notas_de_5+"\n"
+                                          +"Quantidade de notas de 10 : "+notas_de_10+"\n"
+                                          +"Quantidade de notas de 20 : "+notas_de_20+"\n"
+                                          +"Quantidade de notas de 50 : "+notas_de_50+"\n"
+                                          +"Quantidade de notas de 100 : "+notas_de_100+"\n"
+                                          +"Valor solicitado :"+valor_solicitado);
             
         }
         
